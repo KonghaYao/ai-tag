f1d3cdf6396139dbac6257c5ce43676d50c956ed
konghayao <3446798488@qq.com>
2022-10-30 09:29:23 +0800

    fixed: 修复打包错误

--- scripts/fuseIndex.mjs
+++
@@ -1,5 +0,0 @@
-import Fuse from 'fuse.js';
-import fs from 'fs';
-const myIndex = Fuse.createIndex(['title', 'author.firstName'], books);
-// Serialize and save it
-fs.writeFile('fuse-index.json', JSON.stringify(myIndex.toJSON()));

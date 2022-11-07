bf7978060eb6218f6ebad99a7cef0b8f64a1dca4
konghayao <3446798488@qq.com>
2022-11-07 19:08:08 +0800

    feat: feedback i18n

--- src/Feedback/Feedback.tsx
+++ src/Feedback/Feedback.tsx
@@ -1,11 +1,13 @@
 import { atom, useSingleAsync } from '@cn-ui/use';
 import { debounce } from 'lodash-es';
 import { For, useContext } from 'solid-js';
+import { useTranslation } from '../../i18n';
 import { Data } from '../App';
 import { Panel } from '../components/Panel';
 import { Notice } from '../utils/notice';
 import { commitFeedBack, FeedBackMessage, FeedBackTags, Labels } from './index';
 export const FeedBackPanel = () => {
+    const { t } = useTranslation();
     const { visibleId } = useContext(Data);
     /** 反馈信息 */
     const callbacks = atom<FeedBackMessage>({
@@ -22,7 +24,7 @@ export const FeedBackPanel = () => {
     const Commit = useSingleAsync(() => {
         if (checks()) {
             const form = callbacks();
-            Notice.success('提交中。。。');
+            Notice.success(t('feedback.hint.uploading'));
             callbacks((i) => ({
                 ...i,
                 title: '',
@@ -35,26 +37,26 @@ export const FeedBackPanel = () => {
                     url: res.html_url,
                 });
                 localStorage.setItem('__my_feedback__', JSON.stringify(data));
-                Notice.success('提交成功, 感谢您的反馈');
+                Notice.success(t('feedback.hint.success'));
             });
-        } else Notice.error('请完成填写');
+        } else Notice.error(t('feedback.hint.notFilled'));
     });
     return (
         <Panel id="feedback">
-            <h3 class="my-2 text-center text-lg font-bold">反馈信息</h3>
+            <h3 class="my-2 text-center text-lg font-bold">{t('feedback.feedbackMessage')}</h3>
             <div class="flex flex-col gap-2 p-4">
                 <div
                     class="cursor-pointer border border-dashed border-green-800 py-2 text-center text-white transition-colors hover:bg-gray-800"
                     onClick={() => visibleId('my-feedback')}
                 >
-                    查看以前的反馈
+                    {t('feedback.viewOldFeedback')}
                 </div>
 
                 <div class="p-2 text-sm ">
-                    <div>反馈之前，请确认：青少年模式导致词缺失</div>
+                    <div>{t('feedback.beforeFeedback')}</div>
                 </div>
                 <div class="flex justify-between">
-                    <span class="flex-none">我就想要</span>
+                    <span class="flex-none">{t('feedback.IWantTo')}</span>
                     <select
                         value={callbacks().labels[0]}
                         class="ml-2 w-full appearance-none bg-gray-800 px-2 text-sm outline-none"
@@ -74,9 +76,9 @@ export const FeedBackPanel = () => {
                     </select>
                 </div>
                 <div class="flex ">
-                    <span class="flex-none">问题标题</span>
+                    <span class="flex-none">{t('feedback.QuestionTitle')}</span>
                     <input
-                        placeholder="必填"
+                        placeholder={t('feedback.necessary')}
                         value={callbacks().title}
                         class="ml-4 w-full appearance-none rounded-md bg-gray-800 px-2 text-sm outline-none"
                         onChange={(e: any) => {
@@ -88,9 +90,9 @@ export const FeedBackPanel = () => {
                     ></input>
                 </div>
                 <div class="flex ">
-                    <span class="flex-none">你的名称</span>
+                    <span class="flex-none">{t('feedback.YourName')}</span>
                     <input
-                        placeholder="选填，可以留下地址"
+                        placeholder={t('feedback.Path')}
                         value={callbacks().author}
                         class="ml-4 w-full appearance-none rounded-md bg-gray-800 px-2 text-sm outline-none"
                         onChange={(e: any) => {
@@ -102,10 +104,10 @@ export const FeedBackPanel = () => {
                     ></input>
                 </div>
                 <div>
-                    具体描述
+                    {t('feedback.DescriptionTitle')}
                     <textarea
                         value={callbacks().body}
-                        placeholder="必填，请写下相关的信息，相关的来源，触发的条件等，有啥写啥"
+                        placeholder={t('feedback.Description')}
                         class="w-full appearance-none rounded-md bg-gray-800 px-2 text-sm outline-none"
                         onChange={(e: any) => {
                             callbacks((i) => ({
@@ -121,7 +123,7 @@ export const FeedBackPanel = () => {
                 class="cursor-pointer bg-green-700 p-2 text-center font-bold text-white"
                 onclick={Commit}
             >
-                提交，感谢您的支持！
+                {t('feedback.UploadButton')}
             </div>
         </Panel>
     );

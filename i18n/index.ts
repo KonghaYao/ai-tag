9423904ebb7ee10f3582faf85f17f485ed6cc59f
konghayao <3446798488@qq.com>
2022-11-13 14:37:46 +0800

    feat: i18n 翻译完成

--- i18n/index.ts
+++ i18n/index.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import zh from '../locales/zh-CN.json';
 import en from '../locales/en.json';
-import jp from '../locales/jp.json';
+import jp from '../locales/ja.json';
 i18n.use(LanguageDetector).init({
     resources: {
         en: { translation: en },

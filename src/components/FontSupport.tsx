2a1511fd9ee7d51a10020ecee5e631fff8c1ba07
konghayao <3446798488@qq.com>
2022-11-15 19:15:21 +0800

    feat: 改善 font 地址

--- src/components/FontSupport.tsx
+++ src/components/FontSupport.tsx
@@ -17,7 +17,10 @@ export const FontSupport: Component<{ show?: boolean | Atom<boolean>; delay?: nu
                     rel="stylesheet"
                     href="https://cdn.jsdelivr.net/gh/KonghaYao/chinese-free-web-font-storage/build/江西拙楷2.0/result.css"
                 ></link>
-                <link rel="stylesheet" href="https://unpkg.com/@fontsource/aclonica/400.css"></link>
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/@fontsource/aclonica@4.5.9/400.css"
+                ></link>
             </Show>
         </>
     );

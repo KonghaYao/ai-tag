8209aa4b415ce8396b06fb96d9788074784ff6df
江夏尧 <3446798488@qq.com>
2022-12-10 10:55:50 +0800

    feat: 添加下载功能

--- gallery/Gallery.tsx
+++ gallery/Gallery.tsx
@@ -27,6 +27,7 @@ export const Gallery = (props) => {
         </div>
     );
 };
+import { saveAs } from 'file-saver';
 export const GalleryColumn: Component<{ images: StoreData[] }> = (props) => {
     const { visibleId } = useContext(PanelContext);
     const { ShowingPicture, getViewer, backgroundImage } = useContext(GalleryGlobal);
@@ -36,16 +37,7 @@ export const GalleryColumn: Component<{ images: StoreData[] }> = (props) => {
             <For each={props.images} fallback={<div>结果为空</div>}>
                 {(item, index) => {
                     return (
-                        <div
-                            class="single-pic relative  m-auto  cursor-pointer  rounded-md  shadow-lg transition-transform duration-500"
-                            onclick={() => {
-                                batch(() => {
-                                    ShowingPicture(item);
-                                    visibleId('detail');
-                                    backgroundImage(getImagePath(item.image));
-                                });
-                            }}
-                        >
+                        <div class="single-pic relative m-auto w-full  cursor-pointer  rounded-md  shadow-lg transition-transform duration-500">
                             <img
                                 loading="lazy"
                                 src={getImagePath(item.image)}
@@ -54,18 +46,44 @@ export const GalleryColumn: Component<{ images: StoreData[] }> = (props) => {
                                 style={{
                                     'min-height': '6rem',
                                 }}
+                                onclick={() => {
+                                    batch(() => {
+                                        ShowingPicture(item);
+                                        visibleId('detail');
+                                        backgroundImage(getImagePath(item.image));
+                                    });
+                                }}
                             />
-                            <div class="title-item  absolute  bottom-0 left-0 flex w-full flex-col items-center  justify-between bg-slate-200/80 px-4 py-2 text-slate-700">
-                                <div class="py-1 font-bold line-clamp-1">{item.description}</div>
-                                <div class="flex w-full flex-row items-center justify-between">
-                                    <div class="h-fit text-xs line-clamp-1">{item.username}</div>
+                            <div class="absolute top-2 right-2 h-fit rounded-xl bg-lime-600 px-1  text-slate-200  line-clamp-1">
+                                {item.username}
+                            </div>
+                            <div class="title-item  absolute  bottom-0 left-0 flex w-full  items-center  justify-between px-4 py-2 text-slate-700">
+                                <div class="w-fit rounded-lg bg-slate-200 px-2  text-center  line-clamp-1">
+                                    {item.description}
+                                </div>
+
+                                <nav class="flex gap-2">
                                     <div
-                                        class="font-icon cursor-pointer  text-lg"
+                                        class="font-icon h-7 w-7 cursor-pointer rounded-full bg-lime-500  text-center text-lg  text-white"
                                         onclick={() => getViewer().view(index())}
                                     >
                                         photo
                                     </div>
-                                </div>
+                                    <div
+                                        class="font-icon h-7 w-7 cursor-pointer rounded-full bg-lime-500  text-center text-lg  text-white"
+                                        onclick={async () => {
+                                            const data = await fetch(getImagePath(item.image)).then(
+                                                (res) => res.blob()
+                                            );
+                                            saveAs(
+                                                data,
+                                                item.description + '-' + item.username + '.png'
+                                            );
+                                        }}
+                                    >
+                                        download
+                                    </div>
+                                </nav>
                             </div>
                         </div>
                     );

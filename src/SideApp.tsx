d5dc609463e6893d48bc0fa2384abed9a4e42e26
江夏尧 <3446798488@qq.com>
2022-12-24 08:54:48 +0800

    feat: 更改为 Tabs 组件

--- src/SideApp.tsx
+++ src/SideApp.tsx
@@ -10,6 +10,7 @@ import { MyFeedBackPanel } from './Feedback/MyFeedback';
 import { MessageHint } from './MessageHint';
 import { AIPrompt } from './Panels/AIPrompt';
 import { PromptExtractorPanel } from './Panels/PromptExtractor';
+import { Tabs } from '@cn-ui/core';
 export type PanelIds =
     | 'setting'
     | 'ai-prompt'
@@ -21,31 +22,34 @@ export type PanelIds =
     | 'prompt-extractor'
     // gallery 的 panel
     | 'detail';
+
 export const SideApp = () => {
     const { sideAppMode, visibleId } = useContext(Data);
     const hasOpened = createMemo(() => visibleId() !== null);
 
     return (
-        <main
-            class="flex h-full w-1/3 max-w-sm flex-none flex-col bg-gray-900 text-gray-400 transition-all duration-300"
-            classList={{
-                'flex-1': hasOpened(),
-                'w-0': !sideAppMode() || !hasOpened(),
-            }}
-        >
-            <div class="flex-1" classList={{ relative: sideAppMode() }}>
-                <SettingPanel></SettingPanel>
-                <PublicPanel></PublicPanel>
-                <UploadPanel></UploadPanel>
-                <HomePanel></HomePanel>
-                <Webview></Webview>
+        <Tabs activeId={visibleId} lazyload>
+            <main
+                class="flex h-full w-1/3 max-w-sm flex-none flex-col bg-gray-900 text-gray-400 transition-all duration-300"
+                classList={{
+                    'flex-1': hasOpened(),
+                    'w-0': !sideAppMode() || !hasOpened(),
+                }}
+            >
+                <div class="flex-1" classList={{ relative: sideAppMode() }}>
+                    <SettingPanel></SettingPanel>
+                    <PublicPanel></PublicPanel>
+                    <UploadPanel></UploadPanel>
+                    <HomePanel></HomePanel>
+                    <Webview></Webview>
 
-                <FeedBackPanel></FeedBackPanel>
-                <MyFeedBackPanel></MyFeedBackPanel>
-                <AIPrompt></AIPrompt>
-                <PromptExtractorPanel></PromptExtractorPanel>
-            </div>
-            <MessageHint></MessageHint>
-        </main>
+                    <FeedBackPanel></FeedBackPanel>
+                    <MyFeedBackPanel></MyFeedBackPanel>
+                    <AIPrompt></AIPrompt>
+                    <PromptExtractorPanel></PromptExtractorPanel>
+                </div>
+                <MessageHint></MessageHint>
+            </main>
+        </Tabs>
     );
 };
--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -9,9 +9,11 @@ import {
     JSXElement,
     Show,
     useContext,
+    useTransition,
 } from 'solid-js';
 import { PanelIds } from '../SideApp';
 import { ControlBar } from './ControlBar';
+import { Tab } from '@cn-ui/core';
 
 export interface IPanelData {
     visibleId: Atom<string>;
@@ -24,45 +26,34 @@ interface PanelEl extends JSX.HTMLAttributes<HTMLDivElement> {
     children?: JSXElement;
     id: PanelIds | '';
 }
-
 export const Panel: Component<PanelEl> = (props) => {
     const { visibleId, isPanelVisible } = useContext(PanelContext);
     let container: HTMLDivElement;
     const visible = createMemo(() => isPanelVisible(props.id));
-    const hasOpened = createMemo(() => visibleId() !== null);
-    const hidden = atom(!visible());
-    createEffect(() => {
-        if (visible()) hidden(false);
-    });
+
     return (
-        <nav
-            ref={container}
-            class="absolute top-0 left-0 flex h-full w-full flex-col items-center justify-center p-6 transition-all duration-500"
-            classList={{
-                'scale-100': visible(),
-                'scale-0': !visible(),
-                'translate-y-full': !visible(),
-                'pointer-event-none': !visible(),
-                hidden: hidden(),
-            }}
-            onTransitionEnd={() => {
-                // console.log(!visible());
-                hidden(!visible());
-            }}
-            ondragover={props.ondragover}
-            ondrop={props.ondrop}
-            onClick={(e) => {
-                if (e.target === container) visibleId(null);
-                // console.log(e);
-            }}
-        >
-            {/* blur-background 因为 tailwind 的方式有 BUG */}
-            <main class="blur-background flex h-full w-full max-w-sm flex-col  overflow-auto rounded-2xl border-2 border-solid border-slate-700 bg-gray-700/60 transition-all sm:max-w-md">
-                {props.children}
-                <Show when={hasOpened()}>
+        <Tab id={props.id}>
+            <nav
+                ref={container}
+                class="absolute top-0 left-0 flex h-full w-full flex-col items-center justify-center p-6 transition-all duration-500"
+                classList={{
+                    'scale-100': visible(),
+                    'scale-0': !visible(),
+                    'translate-y-full': !visible(),
+                    'pointer-event-none': !visible(),
+                }}
+                ondragover={props.ondragover}
+                ondrop={props.ondrop}
+                onClick={(e) => {
+                    if (e.target === container) visibleId(null);
+                }}
+            >
+                <main class="blur-background flex h-full w-full max-w-sm flex-col  overflow-auto rounded-2xl border-2 border-solid border-slate-700 bg-gray-700/60 transition-all sm:max-w-md">
+                    {props.children}
+
                     <ControlBar></ControlBar>
-                </Show>
-            </main>
-        </nav>
+                </main>
+            </nav>
+        </Tab>
     );
 };

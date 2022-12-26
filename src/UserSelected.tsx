88bd0a2423fa780bc8189d8a79a9d777b7393299
江夏尧 <3446798488@qq.com>
2022-12-26 16:23:09 +0800

    fixed: 修复没有 scrollbar 问题

--- src/UserSelected.tsx
+++ src/UserSelected.tsx
@@ -60,7 +60,7 @@ export const UserSelected = () => {
             <main class="user-selected  my-2 flex w-full flex-col rounded-xl border border-solid border-gray-600 p-2 ">
                 <HeaderFirst></HeaderFirst>
                 <SortableList
-                    class="flex flex-wrap overflow-y-auto overflow-x-hidden text-sm "
+                    class="scroll-box flex flex-wrap overflow-y-auto overflow-x-hidden text-sm"
                     style={{
                         'max-height': '40vh',
                         'min-height': '10vh',

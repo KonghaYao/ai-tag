f093988c03bbc89a3364e22826fa190ddd2cd1a9
江夏尧 <3446798488@qq.com>
2022-12-26 15:28:51 +0800

    refractor: 更新样式

--- src/GlobalHeader.tsx
+++ src/GlobalHeader.tsx
@@ -8,13 +8,23 @@ export function GlobalHeader() {
     const { t } = useTranslation();
     return (
         <header
-            class={`flex  items-baseline  rounded-lg  bg-slate-800 px-4 py-2 text-xl font-bold text-white ring-1 ring-slate-700 `}
+            class={`flex  items-baseline  rounded-lg  bg-slate-800 px-4 py-2 text-xl font-bold text-white`}
         >
-            <h2>魔导绪论</h2>
+            <h2 class="text-slate-200">魔导绪论</h2>
             <sup class="px-2 text-xs text-yellow-300">{__version__}</sup>
-            <sub class="h-fit text-xs">AI 绘画三星法器</sub>
+            <sub class="h-fit text-xs text-slate-200">AI 绘画三星法器</sub>
             <div class="flex-1"></div>
             <div class="flex cursor-pointer items-center  justify-center gap-2  text-xs font-thin text-[#f5f3c2]">
+                {!r18Mode() && (
+                    <span
+                        class="btn bg-green-700"
+                        onClick={() => {
+                            Notice.success(t('header.hint.teen'));
+                        }}
+                    >
+                        {t('header.TeenagerMode')}
+                    </span>
+                )}
                 <a
                     href="https://magic-tag.notion.site/ee1a0ab136724eb183a29d1fcc56a3d2"
                     target="_blank"
@@ -35,16 +45,6 @@ export function GlobalHeader() {
                 <a href="https://github.com/KonghaYao/ai-tag" target="_blank">
                     {'{{ By 江夏尧 }}'}
                 </a>
-                {!r18Mode() && (
-                    <span
-                        class="btn bg-green-700"
-                        onClick={() => {
-                            Notice.success(t('header.hint.teen'));
-                        }}
-                    >
-                        {t('header.TeenagerMode')}
-                    </span>
-                )}
             </div>
         </header>
     );

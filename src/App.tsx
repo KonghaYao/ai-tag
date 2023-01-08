245de453bfdce5226d9c057c0c6884a38867c8dd
江夏尧 <3446798488@qq.com>
2023-01-08 20:27:26 +0800

    feat: 更新词库版本

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ export const App = () => {
     const nonBreakLine = atom<boolean>(false);
     const forceEN = atom<boolean>(false);
     const showClassify = atom<boolean>(true);
-    const tag_version = atom('2.0.3');
+    const tag_version = atom('2.1.0');
 
     /** 需要持久化的变量写这里 */
     const storageSetting = {

8ca51e5093bac1cd26cab889da31a75d571a341e
江夏尧 <3446798488@qq.com>
2023-01-13 10:49:35 +0800

    style: 更改颜色色阶

--- src/ToolBar/sub/ToolBox.tsx
+++ src/ToolBar/sub/ToolBox.tsx
@@ -17,7 +17,7 @@ export const ToolBox = () => {
             popup={() => (
                 <div class="blur-background pointer-events-auto flex flex-col gap-2 rounded-md p-2">
                     {/* 中英文切换符号 */}
-                    <span class="btn bg-yellow-700 text-sm" onclick={() => enMode((i) => !i)}>
+                    <span class="btn bg-blue-700 text-sm" onclick={() => enMode((i) => !i)}>
                         {iconBtn()
                             ? t('toolbar1.' + (enMode() ? 'en' : 'zh'))[0]
                             : t('toolbar1.' + (enMode() ? 'en' : 'zh'))}
@@ -41,11 +41,11 @@ export const ToolBox = () => {
                     >
                         清空所有 TAG
                     </span>
-                    <span class="btn  bg-violet-700 text-sm " onClick={() => undo()}>
+                    <span class="btn  bg-purple-700 text-sm " onClick={() => undo()}>
                         撤销
                     </span>
                     {/* 清空所有 TAG  */}
-                    <span class="btn  bg-violet-700 text-sm " onClick={() => redo()}>
+                    <span class="btn  bg-fuchsia-700 text-sm " onClick={() => redo()}>
                         重做
                     </span>
                 </div>

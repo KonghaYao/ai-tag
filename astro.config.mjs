e756325ec70eb82dbb014e78caef97324e25a883
江夏尧 <3446798488@qq.com>
2023-03-29 17:01:11 +0800

    chore: 更改一些配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import solidJs from '@astrojs/solid-js';
 import tailwind from '@astrojs/tailwind';
 import p from './package.json';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs(), tailwind(), sitemap()]
-  // output: 'server',
+    site: 'https://magic-tag.netlify.app',
+    integrations: [solidJs(), tailwind(), sitemap()],
+    // output: 'server',
 });

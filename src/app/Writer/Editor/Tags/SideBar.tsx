3e675f54619f9279433d9c3b2c4bc8b2d8b1c03d
江夏尧 <3446798488@qq.com>
2023-03-29 10:00:34 +0800

    feat: 添加 tag 的文段

--- src/app/Writer/Editor/Tags/SideBar.tsx
+++ src/app/Writer/Editor/Tags/SideBar.tsx
@@ -2,8 +2,13 @@ import type { Component } from 'solid-js';
 import type { BaseBlock } from '../../interface';
 import { GlobalData } from '../../../../store/GlobalData';
 import { Transformers } from '../Common/Transformers';
+import { AISupport } from '../Common/AISupport';
+import type { Atom, atom } from '@cn-ui/use';
+import type { GlobalGPT } from '../../../../api/prompt-gpt';
 
-export const SideBar: Component<{ block: BaseBlock }> = (props) => {
+export const SideBar: Component<{ block: BaseBlock; model: Atom<keyof typeof GlobalGPT> }> = (
+    props
+) => {
     const { emphasizeAddMode, deleteMode, changeTagMode } = GlobalData.getApp('data');
     return (
         <>
@@ -27,7 +32,7 @@ export const SideBar: Component<{ block: BaseBlock }> = (props) => {
             >
                 ❌
             </li>
-
+            <AISupport model={props.model} block={props.block}></AISupport>
             <Transformers block={props.block}></Transformers>
         </>
     );
--- src/app/Writer/Editor/TagsEditor.tsx
+++ src/app/Writer/Editor/TagsEditor.tsx
@@ -1,25 +1,44 @@
-import { Component, Show } from 'solid-js';
+import { Component, Match, Show, Switch } from 'solid-js';
 import type { BaseBlock } from '../interface';
 import { TagsRow } from '../../main/UserSelected';
-import { atom } from '@cn-ui/use';
-import { stringToTags } from '../../../use/TagsConvertor';
+import { atom, reflect, useEffectWithoutFirst } from '@cn-ui/use';
+import { TagsToString, stringToTags } from '../../../use/TagsConvertor';
 import { useTranslation } from '../../../i18n';
 import { EditorTemplate } from './EditorTemplate';
 import { Footer } from './Tags/Footer';
 import { TagsSearch } from './Tags/TagsSearch';
 import { SideBar } from './Tags/SideBar';
+import type { GlobalGPT } from '../../../api/prompt-gpt';
+import { AIPlace } from './Common/AIPlace';
 
 export const TagsEditor: Component<{ block: BaseBlock }> = (props) => {
     const userCollection = atom(stringToTags(props.block.content.text));
     const inputMode = atom(true);
+    const model = atom<keyof typeof GlobalGPT>('textToText', { equals: false });
+    const showAIPanel = atom(false);
+    const text = reflect(() => TagsToString(userCollection()));
+    useEffectWithoutFirst(() => showAIPanel(true), [model]);
     return (
         <EditorTemplate
-            sideBar={<SideBar block={props.block}></SideBar>}
+            sideBar={<SideBar model={model} block={props.block}></SideBar>}
             footer={<Footer userCollection={userCollection} inputMode={inputMode}></Footer>}
             tool={
-                <Show when={inputMode()}>
-                    <TagsSearch userCollection={userCollection}></TagsSearch>
-                </Show>
+                <>
+                    <Show when={inputMode()}>
+                        <TagsSearch userCollection={userCollection}></TagsSearch>
+                    </Show>
+                    <Show when={showAIPanel()}>
+                        <AIPlace
+                            block={props.block}
+                            method={model}
+                            input={text}
+                            onClose={() => showAIPanel(false)}
+                            onConfirm={(ai) => {
+                                //TODO 转换
+                            }}
+                        ></AIPlace>
+                    </Show>
+                </>
             }
         >
             <TagsRow usersCollection={userCollection}></TagsRow>

44fb4e7e2460d0fff877a6ac7e34497ab5375816
江夏尧 <3446798488@qq.com>
2023-04-16 09:27:03 +0800

    feat: 添加活跃用户

--- src/Panels/Talk/index.tsx
+++ src/Panels/Talk/index.tsx
@@ -1,11 +1,13 @@
 import { InputArea } from './InputArea';
 import { CommentList } from './CommentList';
 import { GlobalData } from '../../store/GlobalData';
+import { ActiveUser } from '../../components/ActiveUser';
 export const TalkDefault = () => {
     const store = GlobalData.getApp('talk');
 
     return (
         <section class="flex flex-col border-slate-500" ref={store.scrollContainer}>
+            <ActiveUser></ActiveUser>
             <InputArea></InputArea>
             <CommentList></CommentList>
         </section>
--- src/components/ActiveUser.tsx
+++ src/components/ActiveUser.tsx
@@ -1,16 +1,17 @@
 import { resource } from '@cn-ui/reactive';
-import { Show } from 'solid-js';
 
 export const ActiveUser = () => {
-    const active = resource(() =>
-        fetch('https://tag-server.deno.dev/site/active').then((res) => res.json())
+    const active = resource(
+        () =>
+            fetch('https://tag-server.deno.dev/site/active')
+                .then((res) => res.json())
+                .then((res) => res[0].x),
+        { initValue: 1 }
     );
     return (
-        <Show when={active.isReady()}>
-            <div class="flex gap-1">
-                <span class="h-2 w-2 bg-green-500"></span>
-                <span>{active()} 人</span>
-            </div>
-        </Show>
+        <div class="flex items-center justify-center gap-2">
+            <span class="h-2 w-2 rounded-full bg-green-500"></span>
+            <span>{active()} 人在线</span>
+        </div>
     );
 };

1eddcba4351478d0659a3ae8402955a8dfc5a3fc
江夏尧 <3446798488@qq.com>
2023-07-12 10:35:07 +0800

    fixed: 修复 杀千刀的 deno 改动原生 api

--- server/database.ts
+++ server/database.ts
@@ -1,26 +1,31 @@
-import { bodyParser, cors, Koa, logger, proxy, Router } from "./deps.ts";
-import { init, PromptStoreRouter } from "./router/promptStore.ts";
+const serve = Deno.serve
+globalThis.Deno.serve = async (func: any) => {
+    console.log(func)
+    return serve(func).finished
+}
+const { bodyParser, cors, Koa, logger, proxy, Router } = await import("./deps.ts");
+const { init, PromptStoreRouter } = await import("./router/promptStore.ts");
 
 const app = new Koa();
 const router = new Router();
 router.use(PromptStoreRouter.routes(), PromptStoreRouter.allowedMethods()).get(
-  "/",
-  (ctx) => {
-    ctx.body = "这是魔导绪论的后端，请勿调用！！！！";
-  },
+    "/",
+    (ctx) => {
+        ctx.body = "这是魔导绪论的后端，请勿调用！！！！";
+    },
 );
 app.use(logger())
-  .use(cors({
-    origin: "*",
-  }))
-  .use(
-    bodyParser(),
-  )
-  .use(router.routes()).use(router.allowedMethods());
+    .use(cors({
+        origin: "*",
+    }))
+    .use(
+        bodyParser(),
+    )
+    .use(router.routes()).use(router.allowedMethods());
 
 (async () => {
-  await init();
-  app.listen(80, () => {
-    console.log("服务启动了");
-  });
+    await init();
+    app.listen(80, () => {
+        console.log("服务启动了");
+    });
 })();
--- server/main.ts
+++ server/main.ts
@@ -1,31 +1,37 @@
-import { bodyParser, cors, Koa, logger, proxy, Router } from "./deps.ts";
-import { tagsRouter } from "./router/tags.ts";
+const serve = Deno.serve
+globalThis.Deno.serve = async (func: any) => {
+    console.log(func)
+    return serve(func).finished
+}
+const { bodyParser, cors, Koa, logger, proxy, Router } = await import("./deps.ts");
+const { tagsRouter } = await import("./router/tags.ts");
 
 const app = new Koa();
 const router = new Router();
 router
-  .use("/tags", tagsRouter.routes(), tagsRouter.allowedMethods())
-  .get(
-    "/",
-    (ctx) => {
-      ctx.body = "这是魔导绪论的后端，请勿调用！！！！";
-    },
-  );
+    .use("/tags", tagsRouter.routes(), tagsRouter.allowedMethods())
+    .get(
+        "/",
+        (ctx) => {
+            ctx.body = "这是魔导绪论的后端，请勿调用！！！！";
+        },
+    );
 app.use(logger())
-  .use(cors({
-    origin: "*",
-  }))
-  .use(
-    bodyParser(),
-  )
-  .use(proxy("/openai", {
-    target: "https://api.openai.com",
-    changeOrigin: true,
-    rewrite: (path) => path.replace("/openai", ""),
-    logs: true,
-  }))
-  .use(router.routes()).use(router.allowedMethods());
+    .use(cors({
+        origin: "*",
+    }))
+    .use(
+        bodyParser(),
+    )
+    .use(proxy("/openai", {
+        target: "https://api.openai.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace("/openai", ""),
+        logs: true,
+    }))
+    .use(router.routes()).use(router.allowedMethods());
 
 app.listen(80, () => {
-  console.log("服务启动了");
+
+    console.log("服务启动了");
 });
